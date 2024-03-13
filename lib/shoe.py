29a06fe518b60e271dd1c4ed617c987a48238462
Samuel Chu <samueljchu0126@gmail.com>
2024-03-13 16:57:04 -0400

    OOP Lab complete

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__ (self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = 'old'
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    # def cobble(self):
+    #     print("Your shoe is as good as new!")
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
+
+    
